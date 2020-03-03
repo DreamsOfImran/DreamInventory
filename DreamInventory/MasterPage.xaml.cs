aa8747632ff3cea809f4321dd4c28da6b9042e7f
DreamsOfImran <ibasha66@gmail.com>
2020-03-03 17:37:09 +0530

    first commit

---
+++ DreamInventory/MasterPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DreamInventory
+{
+    public partial class MasterPage : MasterDetailPage
+    {
+        public MasterPage()
+        {
+            InitializeComponent();
+
+            Detail = new NavigationPage(new DashboardPage());
+        }
+
+        void Dashboard_Tapped(System.Object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new DashboardPage());
+        }
+
+        void Judgement_Tapped(System.Object sender, System.EventArgs e)
+        {
+            IsPresented = false;
+            Detail = new NavigationPage(new JudgementPage());
+        }
+    }
+}
