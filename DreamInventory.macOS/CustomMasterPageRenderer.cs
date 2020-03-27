6a0437972f233818d561a0233bb30f31552c37e6
Imran Basha <ibasha66@gmail.com>
2020-03-27 21:38:01 +0530

    Added Custom Master Page (#22)

---
+++ DreamInventory.macOS/CustomMasterPageRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DreamInventory;
+using DreamInventory.macOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ExportRenderer(typeof(CustomMasterPage), typeof(CustomMasterPageRenderer))]
+namespace DreamInventory.macOS
+{
+    public class CustomMasterPageRenderer : MasterDetailPageRenderer
+    {
+        protected override double MasterWidthPercentage => 0.2;
+    }
+}
