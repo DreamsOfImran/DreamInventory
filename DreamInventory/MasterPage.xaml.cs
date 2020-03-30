d159bcbc66f7fe12666ecfa42a29281b59c9b8dc
wolf76 <iamnarey@gmail.com>
2020-03-30 14:14:24 +0530

    Menu selected item BG color

--- DreamInventory/MasterPage.xaml.cs
+++ DreamInventory/MasterPage.xaml.cs
@@ -32,17 +32,50 @@ namespace DreamInventory
         {
             IsPresented = false;
             Detail = new NavigationPage(new CasesPage());
+            CasesMenuTitle.BackgroundColor = Color.FromHex("#4a69ff");
+            CasesMenuLabel.TextColor = Color.White;
+            CasesMenuLabel.FontAttributes = FontAttributes.Bold;
+
+            DefendantsMenuTitle.BackgroundColor = Color.Transparent;
+            DefendantsMenuLabel.TextColor = Color.FromHex("#575757");
+            DefendantsMenuLabel.FontAttributes = FontAttributes.None;
+
+            PlaintiffsMenuTitle.BackgroundColor = Color.Transparent;
+            PlaintiffsMenuLabel.TextColor = Color.FromHex("#575757");
+            PlaintiffsMenuLabel.FontAttributes = FontAttributes.None;
         }
 
         void Plaintiffs_Tapped(object sender, EventArgs e)
         {
             IsPresented = false;
             Detail = new NavigationPage(new PlaintiffsPage());
+            PlaintiffsMenuTitle.BackgroundColor = Color.FromHex("#4a69ff");
+            PlaintiffsMenuLabel.TextColor = Color.White;
+            PlaintiffsMenuLabel.FontAttributes = FontAttributes.Bold;
+
+            CasesMenuTitle.BackgroundColor = Color.Transparent;
+            CasesMenuLabel.TextColor = Color.FromHex("#575757");
+            CasesMenuLabel.FontAttributes = FontAttributes.None;
+
+            DefendantsMenuTitle.BackgroundColor = Color.Transparent;
+            DefendantsMenuLabel.TextColor = Color.FromHex("#575757");
+            DefendantsMenuLabel.FontAttributes = FontAttributes.None;
         }
         void Defendants_Tapped(object sender, EventArgs e)
         {
             IsPresented = false;
             Detail = new NavigationPage(new DefendantsPage());
+            DefendantsMenuTitle.BackgroundColor = Color.FromHex("#4a69ff");
+            DefendantsMenuLabel.TextColor = Color.White;
+            DefendantsMenuLabel.FontAttributes = FontAttributes.Bold;
+
+            CasesMenuTitle.BackgroundColor = Color.Transparent;
+            CasesMenuLabel.TextColor = Color.FromHex("#575757");
+            CasesMenuLabel.FontAttributes = FontAttributes.None;
+
+            PlaintiffsMenuTitle.BackgroundColor = Color.Transparent;
+            PlaintiffsMenuLabel.TextColor = Color.FromHex("#575757");
+            PlaintiffsMenuLabel.FontAttributes = FontAttributes.None;
         }
     }
 }
